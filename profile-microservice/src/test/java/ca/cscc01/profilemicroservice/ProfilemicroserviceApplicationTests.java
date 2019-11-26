3eecdd7ba0f480b9c36b29f65190084d94df6a59
Swarup Srini <gswarup2001@gmail.com>
2019-11-25 20:11:42 -0500

    changed dependencies to spring data

---
+++ profile-microservice/src/test/java/ca/cscc01/profilemicroservice/ProfilemicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.cscc01.profilemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilemicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ song-microservice/src/main/java/ca/cscc01/songmicroservice/SongmicroserviceApplication.java
@@ -0,0 +1,13 @@
+package ca.cscc01.songmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongmicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongmicroserviceApplication.class, args);
+	}
+
+}
